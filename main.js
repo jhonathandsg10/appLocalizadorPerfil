3fda00354db2482e916edb85bb62d84e9c851f3e
jhonathandsg10 <jhonathan.gomes@estudante.ifms.edu.br>
2022-11-25 22:45:45 -0400

    Funcionalidade: Consultar  e exibir dados da API

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import './style.css'
+import '@picocss/pico'
+const formConsultarPerfil = document.querySelector('#consultarPerfil')
+const inputPerfil = formConsultarPerfil.perfil
+const divDados = document.querySelector('#dados')
+const btnConsultarPerfil = document.querySelector('#btnConsultarPerfil')
+
+formConsultarPerfil.addEventListener('submit', function (event) {
+  event.preventDefault() 
+  consultarPerfil(inputPerfil.value)
+})
+
+async function consultarPerfil(perfil){
+  let reponse = await fetch(`https://api.github.com/users/${perfil}`)
+  let dadosPerfil = await reponse.json()
+  if(dadosPerfil.name === undefined){
+    divDados.innerHTML=`<div class="erro">Perfil não encontrado!</div>`
+  }else{
+    divDados.innerHTML = `
+    <img src="${dadosPerfil.avatar_url}" alt="">
+    <h1>${dadosPerfil.name}</h1>
+    <a href="${dadosPerfil.html_url}">Perfil no GitHub</a>
+   `
+  }
+}
